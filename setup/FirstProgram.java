0e4376e86393965bde94ec82bd4bc536b61705ca
Mukul Joshi <mjoshi1819@gmail.com>
2025-04-20 19:15:23 +0530

    Updated

--- setup/FirstProgram.java
+++
@@ -1,9 +0,0 @@
-import java.lang.*;
-
-public class FirstProgram{
-  public static void main(String[] args){
-    System.out.println("Welcome");
-    System.out.println("KG");
-    System.out.println("Coding");
-  }
-}
