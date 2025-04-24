f072a054db98c5fae8fe4b318ac84b6593ab183b
Mukul Joshi <mjoshi1819@gmail.com>
2025-04-24 22:44:34 +0530

    Add Bitwise and OddEvenBitwise classes for bitwise operations demonstration

---
+++ 04- Operators, if-else and number system/02-challenge/OddEvenBitwise.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class OddEvenBitwise {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the number: ");
+    int num = input.nextInt();
+
+    
+  }
+}
