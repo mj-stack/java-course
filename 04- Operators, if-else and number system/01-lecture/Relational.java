3bcab57d3a1caf4fb67141e3852bda4ad53f0b60
Mukul Joshi <mjoshi1819@gmail.com>
2025-04-24 12:22:54 +0530

    Add Logical and Relational classes for discount and driving eligibility calculations

---
+++ 04- Operators, if-else and number system/01-lecture/Relational.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Relational {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Welcome to Driving License Portal: \n");
+    System.out.print("Enter your age: ");
+    int age = input.nextInt();
+
+    if (age >= 18) {
+      System.out.println("You are eligible to drive: ");
+    } else {
+      System.out.println("Beta cycle chalao");
+    }
+  }
+}
