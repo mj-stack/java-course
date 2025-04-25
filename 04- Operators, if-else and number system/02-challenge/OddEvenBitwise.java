3281e0bb812a07815fc2789c561212dee68199a2
Mukul Joshi <mjoshi1819@gmail.com>
2025-04-25 07:17:14 +0530

    Refactor OddEvenBitwise class to improve user prompt and maintain code clarity

--- 04- Operators, if-else and number system/02-challenge/OddEvenBitwise.java
+++ 04- Operators, if-else and number system/02-challenge/OddEvenBitwise.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 public class OddEvenBitwise {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.println("Enter the number: ");
+    System.out.print("Enter the number: ");
     int num = input.nextInt();
 
-    
+    if ((num & 1) == 0) {
+      System.out.println("Number is even");
+    } else {
+      System.out.println("Number is odd");
+    }
   }
 }
