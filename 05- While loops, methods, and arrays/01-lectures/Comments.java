e6040f2208b0c26b288d0de510fd4218e299013f
Mukul Joshi <mjoshi1819@gmail.com>
2025-04-25 20:47:32 +0530

    Add Comments class with main method and example outputs

---
+++ 05- While loops, methods, and arrays/01-lectures/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+  /**
+   * This is my main method
+   * @param args
+   */
+  public static void main(String[] args) {
+    
+
+    // This is important, don't delete this
+    System.out.println("Hello World!");
+
+    /*
+     * This is the first line of the comment
+     * The comment still continues
+     */
+    int a = 5;
+    System.out.println(a);
+  }
+}
