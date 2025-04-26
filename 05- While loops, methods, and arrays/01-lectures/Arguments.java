0dadc70064aa79179d0795c1e1036a53f03403bc
Mukul Joshi <mjoshi1819@gmail.com>
2025-04-26 23:10:25 +0530

    Add Arguments class with a sum method and main method example

---
+++ 05- While loops, methods, and arrays/01-lectures/Arguments.java
@@ -0,0 +1,9 @@
+public class Arguments {
+  public static void main(String[] args) {
+    System.out.println(sum(5, 5));
+  }
+
+  public static int sum(int first, int second) {
+    return first + second;
+  }
+}
