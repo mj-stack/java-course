14dc0dcdff5b67cfd7e357a50d8023b94abfdd51
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-02 11:38:21 +0530

    Add ArrayUtility class for user input and refactor SumAndAvg to utilize it

--- 05- While loops, methods, and arrays/02-challenge/SumAndAvg.java
+++ 05- While loops, methods, and arrays/02-challenge/SumAndAvg.java
@@ -1,19 +1,15 @@
 public class SumAndAvg {
   public static void main(String[] args) {
-    int[] myArray = new int[5];
-    myArray[0] = 1;
-    myArray[1] = 2;
-    myArray[2] = 3;
-    myArray[3] = 4;
-    myArray[4] = 5;
+    System.out.println("Welcome to sum and average: ");
+    int[] numArray = ArrayUtility.inputArray();
 
     int i = 0;
     int sum = 0;
-    while (i < myArray.length) {
-      sum += myArray[i];
+    while (i < numArray.length) {
+      sum += numArray[i];
       i++;
     }
     System.out.println("Sum is: " + sum);
-    System.out.println("Avg is: " + (sum/myArray.length));
+    System.out.println("Avg is: " + (sum/numArray.length));
   }
 }
