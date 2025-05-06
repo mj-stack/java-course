3822fe2539c5f277ae37fb1d785b8fbf4a8a9481
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-06 09:15:55 +0530

    Add ForLoop class for demonstrating the use of for loop in Java

---
+++ 07- Control statements, Math and Strings/01-lectures/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+  public static void main(String[] args) {
+    // int i = 1;
+    // while (i <= 100) {
+    // System.out.println(i);
+    // i++;
+    // }
+
+    for (int i = 0; i <= 10; i++) {
+      System.out.print(i + " ");
+    }
+  }
+}
