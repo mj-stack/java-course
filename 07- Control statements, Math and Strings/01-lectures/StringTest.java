9939c56c4a3058fbce0dddf3dea063675e3c6eed
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-06 23:23:10 +0530

    Add StringTest class to demonstrate StringBuilder usage in Java

---
+++ 07- Control statements, Math and Strings/01-lectures/StringTest.java
@@ -0,0 +1,7 @@
+public class StringTest {
+  public static void main(String[] args) {
+    StringBuilder myName = new StringBuilder("Mukul Joshi");
+    myName.append(" Thanks!");
+    System.out.println(myName);
+  }
+}
