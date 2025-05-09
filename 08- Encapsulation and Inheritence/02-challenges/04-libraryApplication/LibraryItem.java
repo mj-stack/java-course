85da5f4de580148a880a2190b9243aa370c83337
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-09 13:03:23 +0530

    Add LibraryItem, Book, DVD, and Magazine classes for library application

---
+++ 08- Encapsulation and Inheritence/02-challenges/04-libraryApplication/LibraryItem.java
@@ -0,0 +1,13 @@
+public class LibraryItem {
+    private String itemId;
+    private String title;
+    private String author;
+
+    public void checkout() {
+        System.out.println("Checkout");
+    }
+    public void returnItem() {
+        System.out.println("Returning the item");
+    }
+
+}
---
+++ 08- Encapsulation and Inheritence/02-challenges/04-libraryApplication/Magazine.java
@@ -0,0 +1,3 @@
+public class Magazine extends LibraryItem {
+    private String issueNumber;
+}
