f446fc573370a07d1b1bd4d00adec708ff13e539
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-12 10:18:29 +0530

    Refactor Calculator class to improve method structure and exception handling

--- 10- Exception and File Handling/01-lectures/in/kgcoding/exception/Calculator.java
+++ 10- Exception and File Handling/01-lectures/in/kgcoding/exception/Calculator.java
@@ -4,6 +4,22 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        a();
+    }
+
+    private static void a() {
+        b();
+    }
+
+    private static void b() {
+        c();
+    }
+
+    private static void c() {
+        d();
+    }
+
+    private static void d() {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to division calculator \n");
         System.out.println("Please enter your two numbers: ");
@@ -15,10 +31,13 @@ public class Calculator {
             System.out.printf("Result is %d", a[6]);
             a[6] = first / second;
             System.out.printf("Result is %d", a[6]);
-        } catch (ArithmeticException | ArrayIndexOutOfBoundsException exception) {
-            System.out.printf("%s , enter valid values", exception.getMessage());
-        } catch (Throwable throwable) {
+        }
+        // catch (ArithmeticException | ArrayIndexOutOfBoundsException exception) {
+        //     System.out.printf("%s , enter valid values", exception.getMessage());
+        // }
+        catch (Throwable throwable) {
             System.out.println(throwable.getMessage());
+            throw throwable;
         }
     }
 }
