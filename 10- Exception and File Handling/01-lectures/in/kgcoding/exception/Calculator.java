0b9ab3d5ebea7a56c26a5ea8f456faccd1a42768
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-12 09:51:34 +0530

    Add Calculator class for exception handling demonstration

---
+++ 10- Exception and File Handling/01-lectures/in/kgcoding/exception/Calculator.java
@@ -0,0 +1,24 @@
+package in.kgcoding.exception;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to division calculator \n");
+        System.out.println("Please enter your two numbers: ");
+        int first = input.nextInt();
+        int second = input.nextInt();
+
+        try {
+            int[] a = new int[5];
+            System.out.printf("Result is %d", a[6]);
+            a[6] = first / second;
+            System.out.printf("Result is %d", a[6]);
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException exception) {
+            System.out.printf("%s , enter valid values", exception.getMessage());
+        } catch (Throwable throwable) {
+            System.out.println(throwable.getMessage());
+        }
+    }
+}
