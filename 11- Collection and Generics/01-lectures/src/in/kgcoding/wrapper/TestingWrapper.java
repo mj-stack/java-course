2d053dae9883dd42a984d962c8a1e441b36a0ce1
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-13 10:03:07 +0530

    Add TestingWrapper class with a main method for initial testing

---
+++ 11- Collection and Generics/01-lectures/src/in/kgcoding/wrapper/TestingWrapper.java
@@ -0,0 +1,8 @@
+package in.kgcoding.wrapper;
+
+public class TestingWrapper {
+    public static void main(String[] args) {
+        Integer first = 5;
+        String myName = "Mukul Joshi";
+    }
+}
