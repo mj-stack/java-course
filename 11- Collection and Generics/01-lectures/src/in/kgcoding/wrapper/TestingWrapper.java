5f52cf9e22dae2f12de8827fc2316db77f99466f
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-13 10:06:06 +0530

    Enhance TestingWrapper with additional print statements for variable output

--- 11- Collection and Generics/01-lectures/src/in/kgcoding/wrapper/TestingWrapper.java
+++ 11- Collection and Generics/01-lectures/src/in/kgcoding/wrapper/TestingWrapper.java
@@ -3,6 +3,10 @@ package in.kgcoding.wrapper;
 public class TestingWrapper {
     public static void main(String[] args) {
         Integer first = 5;
+        System.out.println(first);
         String myName = "Mukul Joshi";
+        System.out.println(myName);
+        int second  = first;
+        System.out.println(second);
     }
 }
