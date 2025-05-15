d257a667120d56831ef1a5c505b8fd4281b7877c
Mukul Joshi <mjoshi1819@gmail.com>
2025-05-15 10:46:44 +0530

    Add PrintTask and executor classes to demonstrate multithreading with different thread pools

---
+++ 12- Multithreading and Executer Service/01-lectures/in/kgcoding/executer/PrintTask.java
@@ -0,0 +1,23 @@
+package in.kgcoding.executer;
+
+public class PrintTask implements Runnable {
+    @Override
+    public void run() {
+        // Print task
+        for (int i = 1; i <= 100; i++) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.printf("%d%c ", i, targetChar);
+        }
+        System.out.printf("\n %s %c task completed", Thread.currentThread().getName(), targetChar);
+    }
+
+    private final char targetChar;
+
+    public PrintTask(char targetChar) {
+        this.targetChar = targetChar;
+    }
+}
